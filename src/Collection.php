d79b6f19187ff70a71c664fb2d784efae5796488
Emily Shepherd <emily@emilyshepherd.me>
2017-12-17 18:28:18 +0000

    Initial Commit

---
+++ src/Collection.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of the Spaark Base package.
+ *
+ * (c) Emily Shepherd <emily@emilyshepherd.me>
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * @package spaark/base
+ * @author Emily Shepherd <emily@emilyshepherd.me>
+ * @license MIT
+ */
+
+namespace Spaark\Base\Collection;
+
+use ArrayAccess;
+use IteratorAggregate;
+use Countable;
+
+/**
+ * Represents an abstract collection of items
+ *
+ * @generic ValueType
+ */
+interface Collection extends ArrayAccess, IteratorAggregate, Countable
+{
+    /**
+     * Returns the size of the collection
+     *
+     * @return int The size of the collection
+     */
+    public function size() : int;
+
+    /**
+     * Checks if the Collection is empty
+     *
+     * @return boolean True if the element is empty
+     */
+    public function empty() : bool;
+
+    /**
+     * Basic implementation of contains
+     *
+     * Should be overridden by datatype-specific implementations for
+     * speed improvements
+     *
+     * @param ValueType $searchFor The key to search for
+     * @return boolean
+     */
+    public function contains($searchFor) : bool;
+
+    /**
+     * Performs a map on the collection
+     *
+     * @param callable $cb The function to use to map the collection
+     * @return Collection The mapped collection
+     */
+    public function map(callable $cb) : Collection;
+
+    /**
+     * Returns the data in this collection as a native PHP array
+     *
+     * @return array
+     */
+    public function toArray() : array;
+}
